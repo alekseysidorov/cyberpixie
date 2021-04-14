f0b006f16d14d7c4360a61e99ebf6373cfaa9e04
Aleksei Sidorov <gorthauer87@yandex.ru>
2021-04-14 22:28:39 +0300

    Swith to SPI based driver

--- ws2812b/src/main.rs
+++ ws2812b/src/main.rs
@@ -6,27 +6,54 @@ use longan_nano::{
         delay::McycleDelay,
         pac::{self},
         prelude::*,
-        time::MegaHertz,
-        timer::Timer,
+        spi::Spi,
     },
     sprintln,
 };
 use smart_leds::{SmartLedsWrite, RGB8};
-use ws2812_timer_delay::Ws2812;
+use ws2812_spi::Ws2812;
 
 use panic_halt as _;
+
+struct TickerLine<C: Iterator<Item = RGB8>, const N: usize> {
+    colors: C,
+    current_led: usize,
+}
+
+impl<C: Iterator<Item = RGB8>, const N: usize> TickerLine<C, N> {
+    fn new(colors: C) -> Self {
+        Self {
+            colors,
+            current_led: N - 1,
+        }
+    }
+
+    pub fn clear_line(&self) -> impl Iterator<Item = RGB8> {
+        core::iter::repeat(RGB8::default()).take(N)
+    }
+
+    #[allow(clippy::needless_lifetimes)]
+    pub fn next_line<'a>(&'a mut self) -> impl Iterator<Item = RGB8> + 'a {
+        self.current_led = (self.current_led + 1) % N;
+        (0..N).map(move |active_led| {
+            if active_led == self.current_led {
+                self.colors.next().unwrap()
+            } else {
+                RGB8::default()
+            }
+        })
+    }
+}
 
 #[riscv_rt::entry]
 fn main() -> ! {
     // Hardware initialization step.
     let dp = pac::Peripherals::take().unwrap();
 
-    let mut rcu = dp.RCU.configure().freeze();
+    let mut rcu = dp.RCU.configure().sysclk(108.mhz()).freeze();
     let mut afio = dp.AFIO.constrain(&mut rcu);
 
     let gpioa = dp.GPIOA.split(&mut rcu);
-    let gpiob = dp.GPIOB.split(&mut rcu);
-
     longan_nano::stdout::configure(
         dp.USART0,
         gpioa.pa9,
@@ -36,47 +63,63 @@ fn main() -> ! {
         &mut rcu,
     );
 
-    let timer = Timer::timer0(dp.TIMER0, MegaHertz(3), &mut rcu);
     let mut delay = McycleDelay::new(&rcu.clocks);
 
-    let mut strip = Ws2812::new(timer, gpiob.pb5.into_push_pull_output());
+    let spi = {
+        let pins = (
+            gpioa.pa5.into_alternate_push_pull(),
+            gpioa.pa6.into_floating_input(),
+            gpioa.pa7.into_alternate_push_pull(),
+        );
+
+        Spi::spi0(
+            dp.SPI0,
+            pins,
+            &mut afio,
+            ws2812_spi::MODE,
+            2800.khz(),
+            &mut rcu,
+        )
+    };
+
+    const LED_COUNT: usize = 25;
+    const LED_BRIGHTNESS: u8 = 5;
+
+    let mut strip = Ws2812::new(spi);
+    strip
+        .write(core::iter::repeat(RGB8::default()).take(144))
+        .ok();
 
     sprintln!("ws2812b configured, sending a couple of colors...");
 
-    let mut data = [RGB8::default(); 4];
-    let empty = [RGB8::default(); 4];
+    let colors = [
+        RGB8 {
+            r: LED_BRIGHTNESS,
+            g: 0,
+            b: 0,
+        },
+        RGB8 {
+            r: 0,
+            g: LED_BRIGHTNESS,
+            b: 0,
+        },
+        RGB8 {
+            r: 0,
+            g: 0,
+            b: LED_BRIGHTNESS,
+        },
+    ]
+    .iter()
+    .copied()
+    .cycle();
 
-    data[0] = RGB8 {
-        r: 0,
-        g: 0,
-        b: 10,
-    };
-    data[1] = RGB8 {
-        r: 0,
-        g: 10,
-        b: 0,
-    };
-    data[2] = RGB8 {
-        r: 10,
-        g: 0,
-        b: 0,
-    };
-    data[3] = RGB8 {
-        r: 5,
-        g: 7,
-        b: 8,
-    };
+    let mut lines = TickerLine::<_, LED_COUNT>::new(colors);
 
-    delay.delay_ms(1000);
-    strip.write(empty.iter().cloned()).unwrap();
+    strip.write(lines.clear_line()).unwrap();
 
     loop {
-        delay.delay_ms(1000);
-        strip.write(data.iter().cloned()).unwrap();
-
-        delay.delay_ms(1000);
-        strip.write(empty.iter().cloned()).unwrap();
-
-        sprintln!("Blink cycle finished");
+        delay.delay_us(20);
+        // delay.delay_ms(100);
+        strip.write(lines.next_line()).unwrap();
     }
 }
