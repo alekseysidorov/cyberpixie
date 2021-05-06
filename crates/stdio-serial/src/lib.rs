e00705dea3656e75177cedf8c4f124f6176c9e5b
Aleksei Sidorov <gorthauer87@yandex.ru>
2021-05-06 23:40:54 +0300

    Use static-box by default for the stdio-serial

--- crates/stdio-serial/src/lib.rs
+++ crates/stdio-serial/src/lib.rs
@@ -1,115 +1,90 @@
 #![no_std]
 
-use core::fmt::{self, Write as _Write};
+#[cfg(not(feature = "without_alloc"))]
+extern crate alloc;
+
+use core::{cell::RefCell, fmt::{self, Write as _Write}, ops::DerefMut};
 
 use embedded_hal::serial::Write;
 
-#[cfg(feature = "alloc")]
-mod inner {
-    extern crate alloc;
+#[cfg(not(feature = "without_alloc"))]
+use alloc::boxed::Box;
+#[cfg(feature = "without_alloc")]
+use static_box::Box;
 
-    use alloc::boxed::Box;
-    use core::{cell::RefCell, fmt};
-    use embedded_hal::serial::Write;
+#[cfg(target_arch = "arm")]
+use cortex_m::interrupt::{self, Mutex};
+#[cfg(target_arch = "riscv32")]
+use riscv::interrupt::{self, Mutex};
 
-    #[cfg(target_arch = "arm")]
-    use cortex_m::interrupt::{self, Mutex};
-    #[cfg(target_arch = "riscv32")]
-    use riscv::interrupt::{self, Mutex};
+#[cfg(not(feature = "without_alloc"))]
+type SerialWriter = Box<dyn Write<u8, Error = fmt::Error> + Send>;
+#[cfg(feature = "without_alloc")]
+type SerialWriter = Box<dyn Write<u8, Error = fmt::Error> + Send, 64>;
 
-    type SerialWriter = Box<dyn Write<u8, Error = fmt::Error> + Send>;
+struct SerialWrapper<T: Write<u8>>(T);
 
-    struct SerialWrapper<T: Write<u8>>(T);
+impl<T: Write<u8>> Write<u8> for SerialWrapper<T> {
+    type Error = fmt::Error;
 
-    impl<T: Write<u8>> Write<u8> for SerialWrapper<T> {
-        type Error = fmt::Error;
-
-        fn write(&mut self, word: u8) -> nb::Result<(), Self::Error> {
-            self.0.write(word).map_err(|err| err.map(|_| fmt::Error))
-        }
-
-        fn flush(&mut self) -> nb::Result<(), Self::Error> {
-            self.0.flush().map_err(|err| err.map(|_| fmt::Error))
-        }
+    fn write(&mut self, word: u8) -> nb::Result<(), Self::Error> {
+        self.0.write(word).map_err(|err| err.map(|_| fmt::Error))
     }
 
-    fn box_serial_writer<W>(writer: W) -> SerialWriter
-    where
-        W: Write<u8> + 'static + Send,
-    {
-        Box::new(SerialWrapper(writer))
-    }
-
-    static STDOUT: Mutex<RefCell<Option<SerialWriter>>> = Mutex::new(RefCell::new(None));
-
-    pub fn init<W>(writer: W)
-    where
-        W: Write<u8> + 'static + Send,
-    {
-        let boxed = box_serial_writer(writer);
-        interrupt::free(|cs| {
-            let mut inner = STDOUT.borrow(cs).borrow_mut();
-            inner.replace(boxed);
-        })
-    }
-
-    pub fn with_writer<F>(f: F) -> nb::Result<(), fmt::Error>
-    where
-        F: FnOnce(&mut (dyn Write<u8, Error = fmt::Error> + 'static)) -> nb::Result<(), fmt::Error>,
-    {
-        interrupt::free(|cs| {
-            let mut inner = STDOUT.borrow(cs).borrow_mut();
-            if let Some(writer) = inner.as_mut() {
-                f(writer.as_mut())
-            } else {
-                Ok(())
-            }
-        })
+    fn flush(&mut self) -> nb::Result<(), Self::Error> {
+        self.0.flush().map_err(|err| err.map(|_| fmt::Error))
     }
 }
 
-#[cfg(not(feature = "alloc"))]
-mod inner {
-    use core::fmt;
+fn box_serial_writer<W>(writer: W) -> SerialWriter
+where
+    W: Write<u8> + 'static + Send,
+{
+    Box::new(SerialWrapper(writer))
+}
 
-    use embedded_hal::serial::Write;
+static STDOUT: Mutex<RefCell<Option<SerialWriter>>> = Mutex::new(RefCell::new(None));
 
-    pub fn init<W>(_writer: W)
-    where
-        W: Write<u8> + 'static + Send,
-    {
-    }
-
-    pub fn with_writer<F>(_f: F) -> nb::Result<(), fmt::Error>
-    where
-        F: FnOnce(&mut (dyn Write<u8, Error = fmt::Error> + 'static)) -> nb::Result<(), fmt::Error>,
-    {
-        Ok(())
-    }
+fn with_writer<F>(f: F) -> nb::Result<(), fmt::Error>
+where
+    F: FnOnce(&mut (dyn Write<u8, Error = fmt::Error> + 'static)) -> nb::Result<(), fmt::Error>,
+{
+    interrupt::free(|cs| {
+        let mut inner = STDOUT.borrow(cs).borrow_mut();
+        if let Some(writer) = inner.as_mut() {
+            f(writer.deref_mut())
+        } else {
+            Ok(())
+        }
+    })
 }
 
 pub fn init<W>(writer: W)
 where
     W: Write<u8> + 'static + Send,
 {
-    inner::init(writer)
+    let boxed = box_serial_writer(writer);
+    interrupt::free(|cs| {
+        let mut inner = STDOUT.borrow(cs).borrow_mut();
+        inner.replace(boxed);
+    })
 }
 
 /// Writes a single byte without blocking.
 pub fn write_byte(word: u8) -> nb::Result<(), fmt::Error> {
-    inner::with_writer(|writer| writer.write(word))
+    with_writer(|writer| writer.write(word))
 }
 
 /// Writes a string to the configured serial port device.
 pub fn write_str(s: &str) -> fmt::Result {
-    nb::block!(inner::with_writer(|writer| writer
+    nb::block!(with_writer(|writer| writer
         .write_str(s)
         .map_err(nb::Error::Other)))
 }
 
 /// Writes a formatted string to the configured serial port device.
 pub fn write_fmt(args: fmt::Arguments) -> fmt::Result {
-    nb::block!(inner::with_writer(|writer| writer
+    nb::block!(with_writer(|writer| writer
         .write_fmt(args)
         .map_err(nb::Error::Other)))
 }
@@ -142,7 +117,7 @@ macro_rules! uprintln {
 }
 
 /// Macro for printing to the configured stdout, without a newline.
-/// 
+///
 /// This method prints only if the `dprint` feature enabled, which is useful
 /// for debugging purposes.
 #[cfg(any(feature = "dprint", doc))]
@@ -163,7 +138,7 @@ macro_rules! dprint {
 }
 
 /// Macro for printing to the configured stdout, with a newline.
-/// 
+///
 /// This method prints only if the `dprint` feature enabled, which is useful
 /// for debugging purposes.
 #[macro_export]
