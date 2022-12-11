b45477dec03993a8ed18700dbbd78325847cb417
Aleksey Sidorov <sauron1987@gmail.com>
2022-12-11 17:02:09 +0400

    First handshake between device and host

--- firmware/esp32c3/src/main.rs
+++ firmware/esp32c3/src/main.rs
@@ -1,14 +1,24 @@
-use std::net::TcpListener;
+use std::{net::TcpListener, time::Duration};
 
 use cyberpixie_esp32c3::wifi::{Config, Wifi};
-use cyberpixie_proto::{nb, DeviceRole, FirmwareInfo, Handshake, Message, Service, SimpleMessage};
-use cyberpixie_std_transport::TcpTransport;
+use cyberpixie_proto::ng::{DeviceInfo, DeviceRole};
+use cyberpixie_std_transport::ng::{NetworkPart, SimpleDevice};
 use esp_idf_hal::prelude::Peripherals;
 use esp_idf_svc::{eventloop::EspSystemEventLoop, log::EspLogger};
 use esp_idf_sys as _;
 use log::info; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
-const DEVICE_ID: [u32; 4] = [6, 6, 6, 6];
+struct DeviceStub;
+
+impl SimpleDevice for DeviceStub {
+    fn device_info(&self) -> DeviceInfo {
+        DeviceInfo {
+            role: DeviceRole::Main,
+            group_id: None,
+            strip_len: Some(36),
+        }
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
@@ -26,41 +36,11 @@ fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:80")?;
     info!("Bound TCP on: {:?}", listener.local_addr());
 
-    let stream = listener.incoming().next().unwrap()?;
-    let address = stream.local_addr()?;
-
-    let mut service = Service::new(TcpTransport::new(address, stream), 640);
+    let mut server = NetworkPart::new(DeviceStub, listener)?;
     info!("Created Service");
     loop {
-        info!("Waiting for next message...");
-        let (address, message) = nb::block!(service.poll_next_message())?;
-        let response = match message {
-            cyberpixie_proto::Message::HandshakeRequest(handshake) => {
-                info!("Got handshake: {:?}", handshake);
-                Some(SimpleMessage::HandshakeResponse(Handshake {
-                    device_id: DEVICE_ID,
-                    group_id: None,
-                    role: DeviceRole::Main,
-                }))
-            }
-            cyberpixie_proto::Message::GetInfo => Some(Message::Info(FirmwareInfo {
-                strip_len: 36,
-                version: [0, 0, 0, 0],
-                images_count: 0,
-                device_id: DEVICE_ID,
-                role: DeviceRole::Main,
-            })),
-
-            other => {
-                info!("Got an unsupported message: {:?}", other);
-
-                None
-            }
-        };
-        service.confirm_message(address)?;
-
-        if let Some(response) = response {
-            service.send_message(address, response)?;
+        if let Err(nb::Error::Other(err)) = server.poll() {
+            panic!("{err}");
         }
     }
 }
