437c984c38a3c692bc907bb0ab7a2a60f1acac73
Aleksey Sidorov <sauron1987@gmail.com>
2023-05-28 13:46:57 +0400

    Add extra deps for Qt App

--- shell.nix
+++ shell.nix
@@ -9,9 +9,12 @@ pkgs.mkShell {
     # Dependencies for the code formatting utility
     dprint
     nixpkgs-fmt
-    qt6.full
-  ] ++ lib.optionals stdenv.isDarwin [
+  ]
+  # Additional frameworks for the Qt application
+  ++ lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.AppKit
     darwin.apple_sdk.frameworks.OpenGL
+    darwin.apple_sdk.frameworks.AGL
   ];
 
   shellHook = "${pkgs.crossBashPrompt}";
