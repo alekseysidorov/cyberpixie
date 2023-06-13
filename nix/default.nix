42b5d61da5ad30fe9236970c45a1753b012b8bc1
Aleksey Sidorov <sauron1987@gmail.com>
2023-06-13 17:02:31 +0400

    Remove an obsolete cargo config extractor

--- nix/default.nix
+++ nix/default.nix
@@ -20,62 +20,6 @@ let
       (final: prev: {
         # Setup Rust toolchain in according with the toolchain file 
         rustToolchain = prev.rust-bin.fromRustupToolchainFile ./../rust-toolchain.toml;
-
-        cargoConfigUtils = {
-          fromFile = filePath:
-            let
-              lib = prev.lib;
-              # Read the cargo config variables
-              config = builtins.fromTOML (builtins.readFile filePath);
-              # Get the build target triple(it should be set)
-              target = config.build.target;
-
-              # Extracts cargo target infix from the given target triple
-              toEnvInfix = cargoBuildTarget: builtins.replaceStrings [ "-" "." ] [ "_" "_" ] (final.lib.toUpper cargoBuildTarget);
-              # Converts a flags array to the env variable
-              mkEnvFromFlags = flags: builtins.concatStringsSep " " flags;
-              unstableFlagsToEnv = flags: cargoTarget: final.lib.attrsets.foldlAttrs
-                (a: name: value: {
-                  "CARGO_UNSTABLE_${toEnvInfix name}" = mkEnvFromFlags value;
-                } // a)
-                { }
-                flags;
-
-              mkEnvFromTargetConfig = config: targetInfix:
-                let
-                  asIs = foo: foo;
-                  mkEnvEntry = name: set: fn:
-                    let
-                      entrySuffix = toEnvInfix name;
-
-                      entryKey = "CARGO_TARGET${targetInfix}_${entrySuffix}";
-                      entryValue = (fn set.${name});
-                    in
-                    lib.optionalAttrs (builtins.hasAttr name set) { "${entryKey}" = entryValue; }
-                  ;
-                in
-                mkEnvEntry "linker" config asIs
-                  // mkEnvEntry "runner" config asIs
-                  // mkEnvEntry "rustflags" config mkEnvFromFlags;
-
-              targetConfig = config.target.${target};
-              targetInfix = "_" + (toEnvInfix target);
-              # Combine the entire env variables
-              env = {
-                # Set the cargo build target
-                CARGO_BUILD_TARGET = target;
-              }
-              # Append target specific variables
-              // (mkEnvFromTargetConfig targetConfig targetInfix)
-              # Append unstable features
-              // lib.optionalAttrs (builtins.hasAttr "unstable" config) (unstableFlagsToEnv config.unstable target)
-              // lib.optionalAttrs (builtins.hasAttr "env" config) config.env
-              ;
-            in
-            {
-              inherit target config env;
-            };
-        };
       })
     ] ++ overlays;
   };
