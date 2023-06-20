20f7222adfda5895373c7d8baefa93ecad90ea74
Aleksey Sidorov <sauron1987@gmail.com>
2023-06-20 20:58:18 +0400

    Add board utility to manipulate boards in the single point

--- boards/esp32/esp32c3/shell.nix
+++ boards/esp32/esp32c3/shell.nix
@@ -3,8 +3,7 @@
 , pkgs ? import ./../../../nix { inherit localSystem; }
 }:
 let
-  # Read a cargo build configuration toml
-  shellPrompt = pkgs.mkBashPrompt "esp32c3";
+  boardTarget = "riscv32imc-unknown-none-elf";
 in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
@@ -15,8 +14,11 @@ pkgs.mkShell {
     cargo-espflash
   ];
 
+  # Force cargo build target to make sure that the vscode will use it as well
+  env.CARGO_BUILD_TARGET = boardTarget;
+
   shellHook = ''
     # Setup nice bash prompt
-    ${shellPrompt}
+    ${pkgs.mkBashPrompt boardTarget}
   '';
 }
--- boards/esp32/esp32s3/shell.nix
+++ boards/esp32/esp32s3/shell.nix
@@ -3,36 +3,36 @@
 , pkgs ? import ./../../../nix { inherit localSystem; }
 }:
 let
-  # Read a cargo build configuration toml
-  shellPrompt = pkgs.mkBashPrompt "esp32s3";
+  boardTarget = "xtensa-esp32s3-none-elf";
 in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     rustBuildHostDependencies
     # Special case for the xtensa toolchain
-    espup
     rustup
+    espup
     # Utilites to flash firmware to the device
     espflash
     cargo-espflash
   ];
 
+  # Force cargo build target to make sure that the vscode will use it as well
+  env.CARGO_BUILD_TARGET = boardTarget;
+  env.RUSTUP_TOOLCHAIN = "esp";
+
   shellHook = ''
     # Install esp toolchain
-    ESP_TARGET_DIR="../target/esp"
+    ESP_TARGET_DIR=$(realpath "../../../target/esp")
     EXPORT_FILE="$ESP_TARGET_DIR/export-esh.sh"
-    if [ ! -f "$EXPORT_FILE" ]; then
-        mkdir -p "$ESP_TARGET_DIR"
-        espup install -f $EXPORT_FILE -t esp32s3
+    if [ -f "$EXPORT_FILE" ]; then
+      espup update
     else
-        espup update
+      mkdir -p "$ESP_TARGET_DIR"
+      espup install -f $EXPORT_FILE -t esp32s3
     fi
     # Export variables
     . $EXPORT_FILE
-    export RUSTUP_TOOLCHAIN="esp"
-    # Force cargo build target to make sure that the vscode will use it as well
-    export CARGO_BUILD_TARGET="xtensa-esp32s3-none-elf"
     # Setup nice bash prompt
-    ${shellPrompt}
+    ${pkgs.mkBashPrompt boardTarget}
   '';
 }
